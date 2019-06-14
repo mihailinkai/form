68c7590485fbdcee71c296c7626ca8564772f8a0
mihailinkai <misha.izgareva@gmail.com>
2019-06-14 18:25:45 +0300

    add database

---
+++ public/javascripts/auth.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+$(function() {
+  // toggle
+  var flag = true;
+  $('.switch-button').on('click', function(e) {
+    e.preventDefault();
+
+    if (flag) {
+      flag = false;
+      $('.register').show('slow');
+      $('.login').hide();
+    } else {
+      flag = true;
+      $('.login').show('slow');
+      $('.register').hide();
+    }
+  });
+
+  // clear
+  $('input').on('focus', function() {
+    $('p.error').remove();
+    $('input').removeClass('error');
+  });
+
+  // register
+  $('.register-button').on('click', function(e) {
+    e.preventDefault();
+
+    var data = {
+      login: $('#register-login').val(),
+      password: $('#register-password').val(),
+      passwordConfirm: $('#register-password-confirm').val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      url: '/api/auth/register'
+    }).done(function(data) {
+      if (!data.ok) {
+        $('.register h2').after('<p class="error">' + data.error + '</p>');
+        if (data.fields) {
+          data.fields.forEach(function(item) {
+            $('input[name=' + item + ']').addClass('error');
+          });
+        }
+      } else {
+        $('.register h2').after('<p class="success">Отлично!</p>');
+      }
+    });
+  });
+});
+
+/* eslint-enable no-undef */
